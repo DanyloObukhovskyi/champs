240a850872e7fdc956d997fc64301c210f8946b3
Valera Vlasyuk <valera.vlasyuk@gmail.com>
2020-09-06 13:11:28 +0300

    added competition page

---
+++ src/Controller/CompetitionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CompetitionController extends AbstractController
+{
+    /**
+      * @Route("/ru/competition", name="competition_index")
+      */
+    public function index()
+    {
+        return $this->render('templates/competition.html.twig', ['router' => 'competition']);
+    }
+
+}
