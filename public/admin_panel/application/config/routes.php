1471ae804b520f5a5af838d233c921e0134a07dc
primadg <valera.vlasyuk@gmail.com>
2020-09-18 12:21:59 +0300

    updated routs

--- public/admin_panel/application/config/routes.php
+++ public/admin_panel/application/config/routes.php
@@ -91,7 +91,7 @@ $route['c-admin/admin/add/(:num)'] = 'add_c/admin/$1';
 $route['c-admin/upload_img/(:num)'] = 'upload_img/index/$1';
 	
 $route['c-admin/ajax/update'] = 'handle_a_request';	
-$route['c-admin/ajax/check/'] = 'handle_a_request/check';
+$route['c-admin/ajax/check'] = 'handle_a_request/check';
 $route['c-admin/refund/(:num)'] = 'refund_c/index/$1';
 
 $route['404_override'] = '';
