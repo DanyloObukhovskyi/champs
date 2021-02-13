1f35db666ef16d10dbe6bca228b4025c0cc573fc
Anton Zaika <antonzaika1993@gmail.com>
2021-02-13 19:49:08 +0200

    [fix] load matches on matches page

--- assets/js/pages/MatchesPage.vue
+++ assets/js/pages/MatchesPage.vue
@@ -31,160 +31,160 @@
 </template>
 
 <script>
-    import Paginate from 'vuejs-paginate'
-    import MatchRow from "../components/matches/MatchRow";
-    import Loader from "../components/helpers/Loader";
-    import TenseSelect from "../components/helpers/TenseSelect";
-    import matchService from "../services/MatchService";
-    import Filters from "../components/filters/Filters";
+import Paginate from 'vuejs-paginate'
+import MatchRow from "../components/matches/MatchRow";
+import Loader from "../components/helpers/Loader";
+import TenseSelect from "../components/helpers/TenseSelect";
+import matchService from "../services/MatchService";
+import Filters from "../components/filters/Filters";
 
-    export default {
-        name: "MatchesPage",
-        components: {
-            TenseSelect,
-            Filters,
-            Loader,
-            MatchRow,
-            Paginate,
+export default {
+    name: "MatchesPage",
+    components: {
+        TenseSelect,
+        Filters,
+        Loader,
+        MatchRow,
+        Paginate,
+    },
+    props: ['type'],
+    data() {
+        return {
+            counts: {},
+            selectMatchesType: 'live',
+            matchTypes: {
+                past: 'Прошедшие',
+                live: 'Активные',
+                future: 'Будующие'
+            },
+            page: 1,
+            load: false,
+            perPage: 20,
+            matches: [],
+            filters: {
+                dateFrom: '21.02.2015',
+                dateTo: '21.02.2022',
+                teamA: null,
+                teamB: null
+            },
+            isLoadAll: false,
+        }
+    },
+    watch: {
+        'selectMatchesType': function () {
+            this.changeFilters();
         },
-        props: ['type'],
-        data() {
-            return {
-                counts: {},
-                selectMatchesType: 'live',
-                matchTypes: {
-                    past: 'Прошедшие',
-                    live: 'Активные',
-                    future: 'Будующие'
-                },
-                page: 1,
-                load: false,
-                perPage: 20,
-                matches: [],
-                filters: {
-                    dateFrom: '21.02.2015',
-                    dateTo: '21.02.2022',
-                    teamA: null,
-                    teamB: null
-                },
-                isLoadAll: false,
-            }
+        'filters.dateFrom': function () {
+            this.changeFilters();
         },
-        watch: {
-            selectMatchesType() {
-                this.page = 1;
-                this.matches = [];
-                this.getMatches();
-            },
-            'filters.dateFrom': function () {
-                this.page = 1;
-                this.matches = [];
-                this.getMatches();
-            },
-            'filters.dateTo': function () {
-                this.page = 1;
-                this.matches = [];
-                this.getMatches();
-            },
-            'filters.teamA': function () {
-                this.page = 1;
-                this.matches = [];
-                this.getMatches();
-            },
-            'filters.teamB': function () {
-                this.page = 1;
-                this.matches = [];
-                this.getMatches();
-            },
+        'filters.dateTo': function () {
+            this.changeFilters();
         },
-        computed: {
-            pagesCount() {
-                return Math.ceil(this.counts[this.selectMatchesType] / this.perPage)
-            },
-            showPaginate() {
-                return Number(this.counts[this.selectMatchesType]) > Number(this.perPage);
-            }
+        'filters.teamA': function () {
+            this.changeFilters();
         },
-        methods: {
-            getMatches() {
-                if (!this.load && !this.isLoadAll) {
-                    this.load = true;
-                    matchService.getMatches(this.selectMatchesType, this.page, this.filters)
-                        .then(data => {
-                            this.counts = data.counts;
+        'filters.teamB': function () {
+            this.changeFilters();
+        },
+    },
+    computed: {
+        pagesCount() {
+            return Math.ceil(this.counts[this.selectMatchesType] / this.perPage)
+        },
+        showPaginate() {
+            return Number(this.counts[this.selectMatchesType]) > Number(this.perPage);
+        }
+    },
+    methods: {
+        changeFilters() {
+            this.page = 1;
+            this.matches = [];
+            this.isLoadAll = false;
 
-                            for (let key in data.matches) {
-                                const matches = data.matches[key];
-
-                                const matchesDay = this.matches.find(
-                                    match => match.date === matches.date
-                                )
-                                if (matchesDay !== undefined) {
-                                    this.matches = this.matches.map(match => {
-                                        if (match.date === matches.date) {
-                                            for (let item of matches.items) {
-                                                match.items.push(item)
-                                            }
-                                        }
-                                        return match;
-                                    })
-                                } else {
-                                    this.matches.push(matches);
-                                }
-                            }
-                            this.load = false;
-                            this.page = this.page + 1;
-                        })
-                }
-            },
-            setPage(page) {
-                this.page = page;
-            },
-            setFilter(data) {
-                this.filters[data.filter] = data.value;
-            },
-            scrollEventTrigger() {
-                const self = this;
-                window.onscroll = () => {
-                    const scrollable = $("body").height() - ($(window).innerHeight() + $(window).scrollTop());
-
-                    if (scrollable <= 10) {
-                        self.getMatches()
-                    }
-                }
-            },
-        },
-        mounted() {
             this.getMatches();
-            this.scrollEventTrigger();
+        },
+        getMatches() {
+            if (!this.load && !this.isLoadAll) {
+                this.load = true;
+                matchService.getMatches(this.selectMatchesType, this.page, this.filters)
+                    .then(data => {
+                        this.counts = data.counts;
 
-            if (this.type && this.matchTypes[this.type]) {
-                this.selectMatchesType = this.type;
+                        if (data.matches.length === 0) {
+                            this.isLoadAll = true;
+                        }
+                        for (let key in data.matches) {
+                            const matches = data.matches[key];
+
+                            const matchesDay = this.matches.find(
+                                match => match.date === matches.date
+                            )
+                            if (matchesDay !== undefined) {
+                                this.matches = this.matches.map(match => {
+                                    if (match.date === matches.date) {
+                                        for (let item of matches.items) {
+                                            match.items.push(item)
+                                        }
+                                    }
+                                    return match;
+                                })
+                            } else {
+                                this.matches.push(matches);
+                            }
+                        }
+                        this.load = false;
+                        this.page = this.page + 1;
+                    })
             }
+        },
+        setPage(page) {
+            this.page = page;
+        },
+        setFilter(data) {
+            this.filters[data.filter] = data.value;
+        },
+        scrollEventTrigger() {
+            const self = this;
+            window.onscroll = () => {
+                const scrollable = $("body").height() - ($(window).innerHeight() + $(window).scrollTop());
+
+                if (scrollable <= 10) {
+                    self.getMatches()
+                }
+            }
+        },
+    },
+    mounted() {
+        this.getMatches();
+        this.scrollEventTrigger();
+
+        if (this.type && this.matchTypes[this.type]) {
+            this.selectMatchesType = this.type;
         }
     }
+}
 </script>
 
 <style scoped>
-	.matches {
-		margin-bottom: 4vw;
-	}
+.matches {
+    margin-bottom: 4vw;
+}
 
-	.matches .title h3 {
-		font-size: 2vw;
-		text-align: center;
-		color: black;
-	}
+.matches .title h3 {
+    font-size: 2vw;
+    text-align: center;
+    color: black;
+}
 
-	.dark .matches .title h3 {
-		color: white;
-	}
+.dark .matches .title h3 {
+    color: white;
+}
 
-	.matches .matches-body .date {
-		font-size: 2vw;
-	}
+.matches .matches-body .date {
+    font-size: 2vw;
+}
 
-	.dark .matches .matches-body .date {
-		color: white;
-	}
+.dark .matches .matches-body .date {
+    color: white;
+}
 </style>
