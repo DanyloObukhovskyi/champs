85b6e0dec293e93322a17ae1d95e879582c8c2d2
Anton Zaika <antonzaika1993@gmail.com>
2021-03-02 15:32:32 +0200

    [fix] lc timetable design

--- assets/js/components/cabinet/user/components/training/Timetable.vue
+++ assets/js/components/cabinet/user/components/training/Timetable.vue
@@ -20,70 +20,72 @@
 </template>
 
 <script>
-    import MoreDetail from "./MoreDetail";
-    import LessonRow from "./LessonRow";
+import MoreDetail from "./MoreDetail";
+import LessonRow from "./LessonRow";
 
-    export default {
-        name: "Timetable",
-        components: {
-            LessonRow,
-            MoreDetail,
+export default {
+    name: "Timetable",
+    components: {
+        LessonRow,
+        MoreDetail,
+    },
+    props: {
+        lessons: {
+            default: {}
         },
-        props: {
-            lessons: {
-                default: {}
-            },
-            isPast: {
-                default: false
-            }
-        },
-        data() {
-            return {
-                activeLesson: null,
-                showMoreDetailModal: false
-            }
-        },
-    }
+        isPast: {
+            default: false
+        }
+    },
+    data() {
+        return {
+            activeLesson: null,
+            showMoreDetailModal: false
+        }
+    },
+}
 </script>
 
 <style scoped lang="scss">
-  @import '../../../../../../css/animations.css';
+@import '../../../../../../css/animations.css';
 
+.lessons {
+  -webkit-animation: animation-translate-right 1500ms linear both;
+  animation: animation-translate-right 1500ms linear both;
+  background-color: #eff0f0;
+  border-radius: .5vw;
+  padding: 2vw 3vw;
+  margin-right: 3%;
+
+  .title {
+    color: #9d9fa0;
+    font-size: 1.5vw;
+    font-weight: 600;
+  }
+
+  .lessons-row {
+    margin-left: 1vw;
+
+    .date-day {
+      color: #ea6620;
+      font-size: 1vw;
+      font-weight: 500;
+      padding: 1.5vw .5vw .5vw .5vw;
+    }
+  }
+}
+
+.dark {
   .lessons {
-	-webkit-animation: animation-translate-right 1500ms linear both;
-	animation: animation-translate-right 1500ms linear both;
-	background-color: #eff0f0;
-	border-radius: .5vw;
-	padding: 2vw 3vw;
-    margin-right: 1.5%;
-	.title {
-	  color: #9d9fa0;
-	  font-size: 1.5vw;
-	  font-weight: 600;
-	}
+    background: rgb(37, 40, 42);
+    background: -moz-linear-gradient(90deg, rgba(37, 40, 42, 1) 0%, rgba(61, 65, 70, 1) 100%);
+    background: -webkit-linear-gradient(90deg, rgba(37, 40, 42, 1) 0%, rgba(61, 65, 70, 1) 100%);
+    background: linear-gradient(90deg, rgba(37, 40, 42, 1) 0%, rgba(61, 65, 70, 1) 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#25282a", endColorstr="#3d4146", GradientType=1);
 
-	.lessons-row {
-	  margin-left: 1vw;
-
-	  .date-day {
-		color: #ea6620;
-		font-size: 1vw;
-		font-weight: 500;
-		padding: 1.5vw .5vw .5vw .5vw;
-	  }
-	}
+    .title {
+      color: white;
+    }
   }
-
-  .dark {
-	.lessons {
-	  background: rgb(37, 40, 42);
-	  background: -moz-linear-gradient(90deg, rgba(37, 40, 42, 1) 0%, rgba(61, 65, 70, 1) 100%);
-	  background: -webkit-linear-gradient(90deg, rgba(37, 40, 42, 1) 0%, rgba(61, 65, 70, 1) 100%);
-	  background: linear-gradient(90deg, rgba(37, 40, 42, 1) 0%, rgba(61, 65, 70, 1) 100%);
-	  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#25282a", endColorstr="#3d4146", GradientType=1);
-	  .title {
-		color: white;
-	  }
-	}
-  }
+}
 </style>
