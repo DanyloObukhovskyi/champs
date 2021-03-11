dc5dc8b7b13de9c33da663afaa22d8247aa0f1b3
Anton Zaika <antonzaika1993@gmail.com>
2021-03-11 22:24:25 +0200

    add changes 113

--- assets/js/pages/MarketplaceTrainerPage.vue
+++ assets/js/pages/MarketplaceTrainerPage.vue
@@ -18,7 +18,7 @@
                     О тренере
                 </div>
                 <div class="about-text">
-                    {{trainer.trainer.method}}
+                    <pre>{{ trainer.trainer.method }}</pre>
                 </div>
             </div>
             <trainer-timetable
@@ -39,140 +39,141 @@
 </template>
 
 <script>
-    import MarketplaceTrainerBanner from "../components/marketplace/MarketplaceTrainerBanner";
-    import TrainerFullRow from "../components/trainers/TrainerFullRow";
-    import MarketplaceService from "../services/MarketplaceService";
-    import Loader from "../components/helpers/Loader";
-    import TrainerTimetable from "../components/trainers/TrainerTimetable";
-    import TrainerReviews from "../components/trainers/TrainerReviews";
+import MarketplaceTrainerBanner from "../components/marketplace/MarketplaceTrainerBanner";
+import TrainerFullRow from "../components/trainers/TrainerFullRow";
+import MarketplaceService from "../services/MarketplaceService";
+import Loader from "../components/helpers/Loader";
+import TrainerTimetable from "../components/trainers/TrainerTimetable";
+import TrainerReviews from "../components/trainers/TrainerReviews";
 
-    const games = [
-        {
-            name: 'cs',
-            title: 'CS:GO',
-            icon: 'cs.png'
-        },
-        {
-            name: 'dota',
-            title: 'DOTA 2',
-            icon: 'dota.png'
-        },
-        {
-            name: 'lol',
-            title: 'LOL',
-            icon: 'lol.jpg'
-        },
-    ];
+const games = [
+    {
+        name: 'cs',
+        title: 'CS:GO',
+        icon: 'cs.png'
+    },
+    {
+        name: 'dota',
+        title: 'DOTA 2',
+        icon: 'dota.png'
+    },
+    {
+        name: 'lol',
+        title: 'LOL',
+        icon: 'lol.jpg'
+    },
+];
 
-    export default {
-        name: "MarketplaceTrainerPage",
-        props: ['social', 'trainerId', 'trainingType', 'paymentType'],
-        components: {
-            TrainerReviews,
-            TrainerTimetable,
-            Loader,
-            TrainerFullRow,
-            MarketplaceTrainerBanner
-        },
-        data() {
-            return {
-                games: games,
-                description: {
-                    individual: {},
-                    group: {},
-                    analytic: {}
-                },
-                trainer: null,
-                load: false,
-                banner: null
-            }
-        },
-        methods: {
-            getTrainingDescription() {
-                MarketplaceService.getTrainingDescription()
-                    .then(data => {
-                        this.description = data;
-                    })
+export default {
+    name: "MarketplaceTrainerPage",
+    props: ['social', 'trainerId', 'trainingType', 'paymentType'],
+    components: {
+        TrainerReviews,
+        TrainerTimetable,
+        Loader,
+        TrainerFullRow,
+        MarketplaceTrainerBanner
+    },
+    data() {
+        return {
+            games: games,
+            description: {
+                individual: {},
+                group: {},
+                analytic: {}
             },
-            getTrainer() {
-                this.load = true;
-
-                MarketplaceService.getTrainer(this.trainerId)
-                    .then(trainer => {
-                        this.trainer = trainer;
-                        this.load = false;
-                    })
-            },
-            updateReviews() {
-                MarketplaceService.getTrainerReviews(this.trainerId)
-                    .then(data => {
-                        this.trainer.reviews = data.reviews;
-                        this.trainer.reviewCount = data.reviewCount;
-                        this.trainer.ratingTotal = data.ratingTotal;
-                        this.trainer.rating = data.rating;
-                    })
-            },
-            getBanner() {
-                MarketplaceService.getTrainerBanner(this.trainerId)
-                    .then(data => {
-                        this.banner = data;
-                    })
-            },
-        },
-        mounted() {
-            this.getBanner();
-            this.getTrainer();
-            this.getTrainingDescription();
+            trainer: null,
+            load: false,
+            banner: null
         }
+    },
+    methods: {
+        getTrainingDescription() {
+            MarketplaceService.getTrainingDescription()
+                .then(data => {
+                    this.description = data;
+                })
+        },
+        getTrainer() {
+            this.load = true;
+
+            MarketplaceService.getTrainer(this.trainerId)
+                .then(trainer => {
+                    this.trainer = trainer;
+                    this.load = false;
+                })
+        },
+        updateReviews() {
+            MarketplaceService.getTrainerReviews(this.trainerId)
+                .then(data => {
+                    this.trainer.reviews = data.reviews;
+                    this.trainer.reviewCount = data.reviewCount;
+                    this.trainer.ratingTotal = data.ratingTotal;
+                    this.trainer.rating = data.rating;
+                })
+        },
+        getBanner() {
+            MarketplaceService.getTrainerBanner(this.trainerId)
+                .then(data => {
+                    this.banner = data;
+                })
+        },
+    },
+    mounted() {
+        this.getBanner();
+        this.getTrainer();
+        this.getTrainingDescription();
     }
+}
 </script>
 
 <style scoped lang="scss">
-    .trainer-page {
-        margin-bottom: 5vw;
+.trainer-page {
+  margin-bottom: 5vw;
 
-        .trainer {
-            .about {
-                padding: 1vw;
-                margin-top: 1vw;
-                background: rgb(255, 255, 255);
-                background: -moz-linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(239, 240, 241, 1) 100%);
-                background: -webkit-linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(239, 240, 241, 1) 100%);
-                background: linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(239, 240, 241, 1) 100%);
-                filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#ffffff", endColorstr="#eff0f1", GradientType=1);
+  .trainer {
+    .about {
+      padding: 1vw;
+      margin-top: 1vw;
+      background: rgb(255, 255, 255);
+      background: -moz-linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(239, 240, 241, 1) 100%);
+      background: -webkit-linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(239, 240, 241, 1) 100%);
+      background: linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(239, 240, 241, 1) 100%);
+      filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#ffffff", endColorstr="#eff0f1", GradientType=1);
 
-                .about-title {
-                    font-size: 1.2vw;
-                    color: #ff6d1d;
-                }
+      .about-title {
+        font-size: 1.2vw;
+        color: #ff6d1d;
+      }
 
-                .about-text {
-                    font-size: 1vw;
-                    color: black;
-                }
-            }
-        }
+      .about-text pre {
+        font-size: 1vw;
+        color: black;
+        font-family: 'Exo 2', sans-serif;
+      }
     }
+  }
+}
 
-    .dark {
-        .trainer-page {
-            .trainer {
-                .about {
-                    background: rgb(61, 65, 70);
-                    background: -moz-linear-gradient(90deg, rgba(61, 65, 70, 1) 25%, rgba(37, 40, 42, 1) 100%);
-                    background: -webkit-linear-gradient(90deg, rgba(61, 65, 70, 1) 25%, rgba(37, 40, 42, 1) 100%);
-                    background: linear-gradient(90deg, rgba(61, 65, 70, 1) 25%, rgba(37, 40, 42, 1) 100%);
-                    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#3d4146", endColorstr="#25282a", GradientType=1);
+.dark {
+  .trainer-page {
+    .trainer {
+      .about {
+        background: rgb(61, 65, 70);
+        background: -moz-linear-gradient(90deg, rgba(61, 65, 70, 1) 25%, rgba(37, 40, 42, 1) 100%);
+        background: -webkit-linear-gradient(90deg, rgba(61, 65, 70, 1) 25%, rgba(37, 40, 42, 1) 100%);
+        background: linear-gradient(90deg, rgba(61, 65, 70, 1) 25%, rgba(37, 40, 42, 1) 100%);
+        filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#3d4146", endColorstr="#25282a", GradientType=1);
 
-                    .about-title {
-                        color: #ff6d1d;
-                    }
-
-                    .about-text {
-                        color: white;
-                    }
-                }
-            }
+        .about-title {
+          color: #ff6d1d;
         }
+
+        .about-text pre{
+          color: white;
+        }
+      }
     }
+  }
+}
 </style>
