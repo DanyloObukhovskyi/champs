0ee5edf1c61cba9c3608ee103f68b761a0531d00
Nikita Miahkoy <niksgreek@gmail.com>
2021-04-27 03:23:26 +0300

    fix discord

--- assets/js/components/header/SubNavbar.vue
+++ assets/js/components/header/SubNavbar.vue
@@ -24,7 +24,7 @@
             </div>
         </div>
         <div class="help d-flex">
-            <a href="https://discord.com/invite/tG4D2Wt" target="_blank">
+            <a href="https://discord.gg/rakSKuE" target="_blank">
                 <svg class="discord" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                      aria-hidden="true"
                      focusable="false"
