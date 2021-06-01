efa33231dbb8cd84e12f5bd2dc8c9e1308404a17
Nikita Miahkoy <niksgreek@gmail.com>
2021-06-02 00:08:11 +0300

    fix time

--- src/Service/Match/MatchService.php
+++ src/Service/Match/MatchService.php
@@ -309,7 +309,7 @@ class MatchService extends EntityService
             "streams" => $this->getMatchStreams($match),
             "isLive" => $match->getLive() ? true : false,
             'type' => $type,
-            'streamTime' => date("H:i", $match->getStartAt()->getTimestamp()) < date('H:i'),
+            'streamTime' => date("H:i", $match->getStartAt()->getTimestamp()) > date('H:i'),
             "slug" => $match->getSlug()
         ];
         $matchFields['statistics'] = $this->getMatchStatistics($match);
