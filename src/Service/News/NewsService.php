d187272e6340d020a3aa97fb72bcdfb0d654cb08
Nikita Miahkoy <niksgreek@gmail.com>
2021-06-01 21:46:51 +0300

    fix news

--- src/Service/News/NewsService.php
+++ src/Service/News/NewsService.php
@@ -84,6 +84,7 @@ class NewsService extends EntityService
             'tags' => $tags,
             'game' => !empty($news->getGame()) ? $news->getGame() : null,
             'date_ru' => $news->getDate()->format('Y') === date('Y') ? self::replaceMonth($news->getDate()->format('d F H:i')) :  self::replaceMonth($news->getDate()->format('d F Y')),
+            'date_ru_msk' => $news->getDate()->format('Y') === date('Y') ? self::replaceMonth($news->getDate()->format('d F H:i')) . ' мск' :  self::replaceMonth($news->getDate()->format('d F Y')),
             'date_ru_with_year' => $news->getDate()->format('Y') === date('Y') ? $news->getDate()->format('d m H_i') : $news->getDate()->format('d m Y'),
             'views' => $news->getViews() ?? 0,
             'commentsCount' => count($news->getComments()),
