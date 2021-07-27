0de89c9575d6a76acfae011876ed505a96d4bb87
Nikita Miahkoy <niksgreek@gmail.com>
2021-07-27 17:25:14 +0300

    fix time

--- src/Service/ScheduleService.php
+++ src/Service/ScheduleService.php
@@ -115,8 +115,8 @@ class ScheduleService extends EntityService
             } else {
                 $dateNow = new DateTime("now", new DateTimeZone(User::DEFAULT_TIMEZONE) );
                 $carbonNow = Carbon::createFromFormat(
-                    "Y-m-d %H:%i:%s",
-                    $dateNow->format("Y-m-d %H:%i:%s")
+                    "Y-m-d H",
+                    $dateNow->format("Y-m-d H")
                 );
                 $carbonNow->addHour($_ENV['LIMITING_BOOKING_LESSON']);
             }
